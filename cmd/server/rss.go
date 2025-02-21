31e81edfc13241d61701f810db1a8c4bc66756af
Francis Begyn <francis@begyn.be>
2025-02-21 22:12:18 +0100

    rework rss

--- cmd/server/rss.go
+++ cmd/server/rss.go
@@ -1,26 +1,24 @@
 package main
 
 import (
+	"log/slog"
 	"net/http"
 	"time"
-
-	"within.website/ln"
 )
 
 var rssTime = time.Now()
-
-const IncrediblySecureSalt = "francisisawesome"
 
 func (s *Site) createFeed(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/rss+xml")
 	err := s.rssFeed.WriteRss(w)
 	if err != nil {
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
-		ln.Error(r.Context(), err, ln.F{
-			"remote_addr": r.RemoteAddr,
-			"action":      "generating_rss",
-			"uri":         r.RequestURI,
-			"host":        r.Host,
-		})
+		slog.Error(
+			"failed to write rss feed",
+			"error", err,
+			"action", "generateRSS",
+			"uri", r.RequestURI,
+			"host", r.Host,
+		)
 	}
 }
